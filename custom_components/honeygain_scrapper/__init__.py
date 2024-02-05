d72575a56aa30cf280aeee22c5d6a9dfce67fccc
Makhuta <matykubla@gmail.com>
2024-02-05 14:29:54 +0100

    Add
    Add:
    - button registering with dispatcher connection

--- custom_components/honeygain_scrapper/__init__.py
+++ custom_components/honeygain_scrapper/__init__.py
@@ -1,16 +1,29 @@
 import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.dispatcher import async_dispatcher_connect
+
 
 from homeassistant.const import (
     Platform,
 )
 
-from .const import DOMAIN
+from .const import DOMAIN, BUTTON_EVENT
 
-PLATFORMS = [Platform.SENSOR]
+PLATFORMS = [
+    Platform.SENSOR,
+    Platform.BUTTON
+]
 
 _LOGGER = logging.getLogger(__name__)
+
+
+def handle_event(data):
+    try:
+        if "success" in data and "credits" in data:
+            print(f'Honeypot was {"" if data["success"] == True else "not "} opened succesfully containing {data["credits"]} credits')
+    except:
+        _LOGGER.exception("Error printing data from Honeypot handler.")
 
 async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     """Set up the HoneyGain Scrapper component."""
@@ -20,6 +33,8 @@ async def async_setup(hass: HomeAssistant, config: dict) -> bool:
 async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
+
+    async_dispatcher_connect(hass, BUTTON_EVENT.format(config_entry.entry_id), handle_event)
 
     await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
 
